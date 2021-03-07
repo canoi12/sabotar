98fa4d7a861155d3e2a19c25b661ee7f9897e683
Linux User <s4b0t@4nxm.odontomais.loc>
2021-03-07 14:13:35 -0300

    update

--- src/sbtar.c
+++ src/sbtar.c
@@ -6,29 +6,6 @@
 #define GROUP_MODE_MASK  0x28
 #define OTHER_MODE_MASK 0x1C0
 
-/* POSIX header */
-struct posix_header_t {
-  struct {
-    char name[100];       /* 0 */
-    char mode[8];         /* 100 */
-    char uid[8];          /* 108 */
-    char gid[8];          /* 116 */
-    char size[12];        /* 124 */
-    char mtime[12];       /* 136 */
-    char chksum[8];       /* 148 */
-    char typeflag;        /* 156 */
-    char linkname[100];   /* 157 */
-    char magic[6];        /* 257 */
-    char version[2];      /* 263 */
-    char uname[32];       /* 265 */
-    char gname[32];       /* 297 */
-    char devmajor[8];     /* 329 */
-    char devminor[8];     /* 337 */
-    char prefix[155];     /* 345 */
-    char _[12];           /* 500 */
-  };                      /* 512 */
-};
-
 static const char *_aux[] = {
   "---", "--x", "-w-", "-wx", 
   "r--", "r-x", "rw-", "rwx"
--- src/sbtar.h
+++ src/sbtar.h
@@ -57,6 +57,29 @@ typedef struct sbtar_t sbtar_t;
 typedef struct sbtar_header_t sbtar_header_t;
 typedef struct posix_header_t posix_header_t;
 
+/* POSIX header */
+struct posix_header_t {
+  struct {
+    char name[100];       /* 0 */
+    char mode[8];         /* 100 */
+    char uid[8];          /* 108 */
+    char gid[8];          /* 116 */
+    char size[12];        /* 124 */
+    char mtime[12];       /* 136 */
+    char chksum[8];       /* 148 */
+    char typeflag;        /* 156 */
+    char linkname[100];   /* 157 */
+    char magic[6];        /* 257 */
+    char version[2];      /* 263 */
+    char uname[32];       /* 265 */
+    char gname[32];       /* 297 */
+    char devmajor[8];     /* 329 */
+    char devminor[8];     /* 337 */
+    char prefix[155];     /* 345 */
+    char _[12];           /* 500 */
+  };                      /* 512 */
+};
+
 struct sbtar_t {
   unsigned offset;
   unsigned size;
